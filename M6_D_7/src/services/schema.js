b58e43c690f9c32c1a8b1c55f19172601b10d274
Abnet Nega <86370531+AbBiz101@users.noreply.github.com>
2021-11-10 13:28:44 +0100

    day 7

--- M6_D_7/src/services/schema.js
+++ M6_D_7/src/services/schema.js
@@ -5,17 +5,17 @@ const { Schema, model } = mongoose;
 const blogSchema = new Schema(
 	{
 		category: { type: String, required: true },
-		// title: { type: String, required: true },
-		// cover: { type: String, required: true },
-		// readTime: {
-		// 	value: { type: Number, required: true },
-		// 	unit: { type: Number, required: true },
-		// },
-		// author: {
-		// 	name: { type: String, required: true },
-		// 	avatar: { type: String, required: true },
-		// },
-		// contect: { type: String, required: true },
+		title: { type: String, required: true },
+		cover: { type: String, required: true },
+		readTime: {
+			value: { type: Number, required: true },
+			unit: { type: Number, required: true },
+		},
+		author: {
+			name: { type: String, required: true },
+			avatar: { type: String, required: true },
+		},
+		contect: { type: String, required: true },
 	},
 	{
 		timestamps: true,
